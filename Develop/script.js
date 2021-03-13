82f872170ced7edc8063142e8736e98eb26720cd
Casey Johnson <johnsoncm3@gmail.com>
2021-03-12 19:17:02 -0500

    Added comments to js file, and updated readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,126 +1,115 @@
 
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate"); 
-// var lowerCase = "abcd";
-// var upperCase = "ABCD";
-// var numberChar = "12345";
-// var specialChar = "!@#$%";
-// // var lowerCase = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" ,"i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , 
-// // "t" , "u" , "v" , "w" , "x" , "y", "z"];
-// // var upperCase = ["A" , "B" , "C" , "D" , "E" , "F" , "G" , "H" , "I" , "J" , "K" , "L" , "M" , "N" , "O" , "P" , "Q" , 
-// // "R" , "S" , "T" , "U" , "V" , "W" , "X" , "Y" , "Z"];
-// // var numberChar = ["0", "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"];
-// // var specialChar = ["!", "@" , "#" , "$" , "%" , "^" , "&" , "*" , "(" , ")" , "_" , "+" , ";" , ";" , "~" , "<" , ">" , 
-// // "." , "?" , "/" , "|"];
-// var passwordLength;
-// var uppercaseCheck;
-// var lowercaseCheck;
-// var numberCheck;
-// var specialCheck; 
 
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+var passwordLength;
+var uppercaseCheck;
+var lowercaseCheck;
+var numberCheck;
+var specialCheck;
+var characters;
+var finalpassword;
+var tempcharacter;
+var password;
 
-// var lengthofpassword = 10;
-// var password = "abcdABCD1234!@#$";
-// var finalpass = "";
+// Determine password length. Ask the user how long they want their password to be
+// making sure that they stay within 8 and 128 characters
 
-// for (var i = 0; i<lengthofpassword; i++){
-//   var randomChar = password.charAt(Math.floor(Math.random() * password.length));
-//   finalpass += randomChar
-// }
-// console.log(finalpass);
-
-
-
-
-// Determine password length
-
-function determineLength(){
-  //Ask user for password length
+function determineLength() {
   var passwordLength = prompt("Enter password length. Min 8 characters and max 128 characters");
 
-    if (passwordLength<8){
-      alert("Password length must be a number between 8-128 characters");
-       determineLength();
-    }else if (passwordLength>128){
-      alert("Password length must be a number between 8-128 characters");
-       determineLength();
-    }else if (isNaN (passwordLength)){
-      alert("Password length must be a number between 8-128 characters");
-      determineLength();
-    }
+  if (passwordLength < 8) {
+    alert("Password length must be a number between 8-128 characters");
+    determineLength();
+  } else if (passwordLength > 128) {
+    alert("Password length must be a number between 8-128 characters");
+    determineLength();
+  } else if (isNaN(passwordLength)) {
+    alert("Password length must be a number between 8-128 characters");
+    determineLength();
+  }
   return passwordLength;
-    
-  
+
+
 }
+// Determine uppercase letters. Ask the user if they want to include uppercase letters in their password.
 
-function determineUppercase(){
-  uppercaseCheck = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
-   
-    if (uppercaseCheck === null || uppercaseCheck === ""){
-      alert("Please answer Yes or No");
-      determineUppercase();
-    }else if (uppercaseCheck === "yes" || uppercaseCheck === "y"){
-      uppercaseCheck = true;
-    }else if (uppercaseCheck === "no" || uppercaseCheck === "n"){
-      uppercaseCheck = false;
-    }else {
-      alert("Please answer Yes or No");
-      determineUppercase();
-    }
-   
-    return uppercaseCheck;
-}
+function determineUppercase() {
+  var uppercaseCheck = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
 
-
-function determineLowercase(){
-  lowercaseCheck = prompt("Do you want to include lowercase letters in your password? \n(Yes or No)");
-   
-    if (lowercaseCheck === null || lowercaseCheck === ""){
-      alert("Please answer Yes or No");
-      determineLowercase();
-    }else if (lowercaseCheck === "yes" || lowercaseCheck === "y"){
-      lowercaseCheck = true;
-    }else if (lowercaseCheck === "no" || lowercaseCheck === "n"){
-      lowercaseCheck = false;
-    }else {
-      alert("Please answer Yes or No");
-      determineLowercase();
-    }
-    
-    return lowercaseCheck;
-}
-
-  function determineNumbers(){
-    numberCheck = prompt("Do you want to include numbers in your password? \n(Yes or No)");
-     
-      if (numberCheck === null || numberCheck === ""){
-        alert("Please answer Yes or No");
-        determineNumbers();
-      }else if (numberCheck === "yes" || numberCheck === "y"){
-        numberCheck = true;
-      }else if (numberCheck === "no" || numberCheck === "n"){
-        numberCheck = false;
-      }else {
-        alert("Please answer Yes or No");
-        determineNumbers();
-      }
-     
-      return numberCheck;
+  if (uppercaseCheck === null || uppercaseCheck === "") {
+    alert("Please answer Yes or No");
+    determineUppercase();
+  } else if (uppercaseCheck === "yes" || uppercaseCheck === "y") {
+    uppercaseCheck = true;
+  } else if (uppercaseCheck === "no" || uppercaseCheck === "n") {
+    uppercaseCheck = false;
+  } else {
+    alert("Please answer Yes or No");
+    determineUppercase();
   }
 
-function determineSpecial(){
-  specialCheck = prompt("Do you want to include special characters in your password? \n(Yes or No)");
+  return uppercaseCheck;
+}
+
+
+// Determine lowercase letters. Ask the user if they want to include lowercase letters in their password.
+
+function determineLowercase() {
+  var lowercaseCheck = prompt("Do you want to include lowercase letters in your password? \n(Yes or No)");
+
+  if (lowercaseCheck === null || lowercaseCheck === "") {
+    alert("Please answer Yes or No");
+    determineLowercase();
+  } else if (lowercaseCheck === "yes" || lowercaseCheck === "y") {
+    lowercaseCheck = true;
+  } else if (lowercaseCheck === "no" || lowercaseCheck === "n") {
+    lowercaseCheck = false;
+  } else {
+    alert("Please answer Yes or No");
+    determineLowercase();
+  }
+
+  return lowercaseCheck;
+}
+
+// Determine numbers. Ask the user if they want to include numbers in their password.
+
+function determineNumbers() {
+  var numberCheck = prompt("Do you want to include numbers in your password? \n(Yes or No)");
+
+  if (numberCheck === null || numberCheck === "") {
+    alert("Please answer Yes or No");
+    determineNumbers();
+  } else if (numberCheck === "yes" || numberCheck === "y") {
+    numberCheck = true;
+  } else if (numberCheck === "no" || numberCheck === "n") {
+    numberCheck = false;
+  } else {
+    alert("Please answer Yes or No");
+    determineNumbers();
+  }
+
+  return numberCheck;
+}
+
+
+// Determine special characters. Ask the user if they want to include special characters in their password.
+
+function determineSpecial() {
+  var specialCheck = prompt("Do you want to include special characters in your password? \n(Yes or No)");
   specialCheck = specialCheck.toLowerCase();
 
-  if (specialCheck === null || specialCheck ===""){
+  if (specialCheck === null || specialCheck === "") {
     alert("Please answer Yes or No");
     determineSpecial();
-  }else if (specialCheck === "yes" || specialCheck === "y"){
+  } else if (specialCheck === "yes" || specialCheck === "y") {
     specialCheck = true;
-  }else if (specialCheck === "no" || specialCheck === "n"){
+  } else if (specialCheck === "no" || specialCheck === "n") {
     specialCheck = false;
-  }else {
+  } else {
     alert("Please answer Yes or No");
     determineSpecial();
   }
@@ -131,65 +120,74 @@ function determineSpecial(){
 
 
 
+// Run the function 'generatePassword' in order to run through the above functions to validate data.
+// The function will then generate the password.
 
+function generatePassword() {
 
-function generatePassword(){
- 
 
   var passwordLength = determineLength();
 
   var lowercaseCheck = determineLowercase();
 
   var uppercaseCheck = determineUppercase();
-  
+
   var numberCheck = determineNumbers();
- 
+
   var specialCheck = determineSpecial();
-  
+
   console.log(passwordLength, lowercaseCheck, uppercaseCheck, specialCheck);
- 
+
+  // Asssign the variable 'characters' to an empty accumulator string in order to 
+  // store the returned characters of (uppercase, lowercase, numbers, and special characters).
+
   var characters = "";
 
-  if (lowercaseCheck){
+  if (lowercaseCheck) {
     characters += "abcdefghijklmnopqrstuvwxyz";
   }
-  if (uppercaseCheck){
+  if (uppercaseCheck) {
     characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  if (numberCheck){
+  if (numberCheck) {
     characters += "0123456789";
   }
-  if (specialCheck){
+  if (specialCheck) {
     characters += "!@#$%^&*()_+~{}:";
   }
 
+  // Create a for loop in order to have the loop run as long as the length of the password that is set. 
+  // This will add a character each time it loops until it satisfys the password length.
 
+  var finalpassword = ""; // setting 'finalpassword' equal to an empty string allows us to store each character in the string.
+  for (var i = 0; i < passwordLength; i++) {
 
+    // Use the 'charAt' method followed by the 'Math.floor....' method to return a randomized character from the string indexs    
+    // on lines 147, 150, 153, and 156 
 
-
-
-  var finalpassword = "";
-  for (var i = 0; i < passwordLength; i++){
     var tempcharacter = characters.charAt(Math.floor(Math.random() * characters.length));
+    
+    // This method adds the values of 'finalpassword' and 'tempcharacter' together
+    
     finalpassword += tempcharacter;
   }
   console.log(finalpassword);
   return finalpassword;
 }
- 
+
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
+ 
+  // This method returns the first element within the document that matches the password id.
+  
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 
 }
 
+// This executes the writePassword function when the user clicks the button
 
-// var generateBtn = document.querySelector("#generate");  //returns the first element in the document that matches the specified selector. Basically, this grabs the 'Generate Password' button;
-// generateBtn.addEventListener("click", generatePassword);  // upon click, executes writePassword function. But why doesn't writePassword need ()?
-
-  // Add event listener to generate button
-  
-  generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # passwordgenerator
+
+[Link to Website] ()
+
+Screenshot
+
+The supplied JavaScript codebase was edited to develop a useable password generator - allowing users to create strong passwords to ensure greater security.
+
+When the user clicks the "Generate Password" button, they are presented with a series of prompts to determine:
+- the length of the password (between 8 and 128 characters)
+- whether they would like uppercase, lowercase, numbers and special characters in their password
+
+Upon completing prompts a password is populated in the box on the page.
+
+The page does not produce any errors in the console when using the inspector in Chrome DevTools.
